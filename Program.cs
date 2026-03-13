3b235644dab913fe7f00dbb6157572771724acfb
sumanthKedarisetti <saisumanth3112@gmail.com>
2026-03-12 20:31:38 -0500

    Added authentication

--- Program.cs
+++ Program.cs
@@ -1,8 +1,54 @@
 using System.Reflection.Metadata.Ecma335;
 using GameStore.API.DTOs;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    options.DefaultScheme=CookieAuthenticationDefaults.AuthenticationScheme;    
+    options.DefaultSignInScheme=CookieAuthenticationDefaults.AuthenticationScheme;  
+
+}).AddCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+    options.Cookie.Name = "GameStoreAuthCookie";
+    // during development we might hit the site over plain HTTP;
+    // a secure cookie policy of Always will prevent the auth cookie from
+    // being stored, which can interfere with the login flow. Use SameAsRequest
+    // or comment this out when testing locally.
+    options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    options.Cookie.SameSite = SameSiteMode.Lax;
+
+}).AddOpenIdConnect(options =>
+{
+    options.Authority = builder.Configuration["keycloak:Authority"];
+    options.ClientId = builder.Configuration["keycloak:ClientId"];
+    options.ClientSecret = builder.Configuration["keycloak:ClientSecret"];
+    options.ResponseType = "code";
+    options.SaveTokens = true;
+    options.CallbackPath = builder.Configuration["keycloak:CallbackPath"];
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.RequireHttpsMetadata=false;
+    options.TokenValidationParameters=new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        NameClaimType="preferred_username"
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    // Enforce authentication for all endpoints by default
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
 
 List<GameDTO> games=[
     new GameDTO(1,"The Legend of Zelda: Breath of the Wild","Action-Adventure",59.99m,new DateTime(2017,3,3),"Nintendo","Nintendo"),
@@ -13,18 +59,21 @@ List<GameDTO> games=[
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 //Get all games
-app.MapGet("/games", () => games);
+
+app.MapGet("/games", () => games).RequireAuthorization();
 //Get game by id
-app.MapGet("/games/{id}",(int id)=>games.Find(g=>g.Id==id)).WithName("GetGame");    
+app.MapGet("/games/{id}",(int id)=>games.Find(g=>g.Id==id)).WithName("GetGame").RequireAuthorization();    
 
 //Create a New Game
 app.MapPost("/games",(CreateGameDTO NewGame) =>
 {
     GameDTO game=new GameDTO(games.Count+1,NewGame.Name,NewGame.Genre,NewGame.Price,DateTime.Now,NewGame.Developer,NewGame.Publisher);
     games.Add(game);  
-});
+}).RequireAuthorization();
 
 app.MapPut("/games/{id}",(int id,CreateGameDTO UpdatedGame)=>
 {
@@ -48,5 +97,30 @@ app.MapDelete("/games/{id}",(int id)=>
     games.RemoveAt(index);
     return Results.NoContent();
 });
+
+// Authentication endpoints
+app.MapGet("/Account/Login", (HttpContext http) =>
+{
+    // Trigger OpenID Connect challenge to sign the user in
+    var props = new AuthenticationProperties { RedirectUri = "/" };
+    // Challenge accepts a collection of schemes, so wrap the single scheme in an array
+    return Results.Challenge(props, new[] { OpenIdConnectDefaults.AuthenticationScheme });
+});
+
+app.MapGet("/Account/Logout", async (HttpContext http) =>
+{
+    // Sign out of both the local cookie and the OpenID Connect provider
+    await http.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    await http.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme,
+        new AuthenticationProperties { RedirectUri = "/" });
+    return Results.Redirect("/");
+});
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// default root endpoint
+app.MapGet("/", () => Results.Redirect("/games"));
+
 app.Run();
  
